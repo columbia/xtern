ad3889de22c8df031a8aa0d56ab35a4fb0917999
Junfeng Yang <junfeng@cs.columbia.edu>
2011-07-19 13:03:56 -0400

    more testcases for the instruction log builder
    should probably use .ll testcases instead of .cpp ones, to avoid random
    factors in gcc/g++

---
+++ test/recorder/main-pthread-exit-test.cpp
@@ -0,0 +1,42 @@
+// RUN: %llvmgcc %s -g -O0 -c -o %t1.ll -S
+// RUN: %projbindir/tern-instr < %t1.ll -o %t2
+// RUN: llc -o %t2.s %t2-record.bc
+// RUN: g++ -g -o %t2 %t2.s -L %projlibdir -lcommonruntime -lrecruntime -lpthread
+// XXX: ./%t2  deadlock if main calls pthread_exit
+// XFAIL
+
+#include <sys/time.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define N (2)
+
+void bar() {
+  pthread_exit(NULL);
+}
+
+void* thread_func(void *arg) {
+  bar();
+}
+
+int main() {
+  int ret;
+  pthread_t th[N];
+
+  for(unsigned i=0; i<N; ++i) {
+    ret  = pthread_create(&th[i], NULL, thread_func, (void*)i);
+    assert(!ret && "pthread_create() failed!");
+  }
+
+  for(unsigned i=0; i<N; ++i)
+    pthread_join(th[i], NULL);
+
+  pthread_exit(NULL);
+  return 0;
+}
---
+++ test/recorder/pthread-exit-test.cpp
@@ -0,0 +1,40 @@
+// RUN: %llvmgcc %s -g -O0 -c -o %t1.ll -S
+// RUN: %projbindir/tern-instr < %t1.ll -o %t2
+// RUN: llc -o %t2.s %t2-record.bc
+// RUN: g++ -g -o %t2 %t2.s -L %projlibdir -lcommonruntime -lrecruntime -lpthread
+// RUN: ./%t2
+
+#include <sys/time.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define N (2)
+
+void bar() {
+  pthread_exit(NULL);
+}
+
+void* thread_func(void *arg) {
+  bar();
+}
+
+int main() {
+  int ret;
+  pthread_t th[N];
+
+  for(unsigned i=0; i<N; ++i) {
+    ret  = pthread_create(&th[i], NULL, thread_func, (void*)i);
+    assert(!ret && "pthread_create() failed!");
+  }
+
+  for(unsigned i=0; i<N; ++i)
+    pthread_join(th[i], NULL);
+
+  return 0;
+}
