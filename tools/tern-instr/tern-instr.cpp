024edb9e25720c4ff652a39c0141125b4fde2b8c
Junfeng Yang <junfeng@cs.columbia.edu>
2011-07-19 22:11:44 -0400

    link with klee uclibc

--- tools/tern-instr/tern-instr.cpp
+++ tools/tern-instr/tern-instr.cpp
@@ -1,15 +1,17 @@
 /* Author: Junfeng Yang (junfeng@cs.columbia.edu) */
-#include "llvm/LLVMContext.h"
+#include "llvm/Linker.h"
 #include "llvm/Module.h"
+#include "llvm/LLVMContext.h"
 #include "llvm/PassManager.h"
 #include "llvm/Analysis/Verifier.h"
+#include "llvm/Bitcode/ReaderWriter.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/Assembly/PrintModulePass.h"
-#include "llvm/Bitcode/ReaderWriter.h"
-#include "llvm/Support/PassNameParser.h"
+#include "llvm/System/Path.h"
 #include "llvm/Support/IRReader.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/PassNameParser.h"
 
 #include "common/id-manager/IDTagger.h"
 
@@ -32,6 +34,10 @@ static cl::opt<std::string>
 OutputFilename("o", cl::desc("Output filename prefix (without .bc or .ll)"),
                cl::value_desc("filename"));
 
+static cl::opt<std::string>
+Uclibc("uclibc", cl::desc("path to klee uclibc library)"),
+       cl::value_desc("filename"));
+
 static cl::opt<bool>
 OutputAssembly("S", cl::desc("Write output as LLVM assembly"));
 
@@ -41,6 +47,136 @@ static bool endsWith(const string& str, const char* suffix) {
   if(len < suffix_len)
     return false;
   return str.compare(len-suffix_len, suffix_len, suffix) == 0;
+}
+
+
+// copied from klee
+Module *linkWithLibrary(Module *module,
+                        const std::string &libraryName) {
+  Linker linker("klee", module, false);
+
+  llvm::sys::Path libraryPath(libraryName);
+  bool native = false;
+
+  if (linker.LinkInFile(libraryPath, native)) {
+    assert(0 && "linking in library failed!");
+  }
+
+  return linker.releaseModule();
+}
+
+
+static llvm::Module *linkWithUclibc(llvm::Module *mainModule) {
+  Function *f;
+  // force import of __uClibc_main
+  mainModule->getOrInsertFunction("__uClibc_main",
+        FunctionType::get(Type::getVoidTy(getGlobalContext()),
+        std::vector<const Type*>(), true));
+
+  f = mainModule->getFunction("__ctype_get_mb_cur_max");
+  if (f) f->setName("_stdlib_mb_cur_max");
+
+  // Strip of asm prefixes for 64 bit versions because they are not
+  // present in uclibc and we want to make sure stuff will get
+  // linked. In the off chance that both prefixed and unprefixed
+  // versions are present in the module, make sure we don't create a
+  // naming conflict.
+  for (Module::iterator fi = mainModule->begin(), fe = mainModule->end();
+       fi != fe;) {
+    Function *f = fi;
+    ++fi;
+    const std::string &name = f->getName();
+    if (name[0]=='\01') {
+      unsigned size = name.size();
+      if (name[size-2]=='6' && name[size-1]=='4') {
+        std::string unprefixed = name.substr(1);
+
+        // See if the unprefixed version exists.
+        if (Function *f2 = mainModule->getFunction(unprefixed)) {
+          f->replaceAllUsesWith(f2);
+          f->eraseFromParent();
+        } else {
+          f->setName(unprefixed);
+        }
+      }
+    }
+  }
+
+  mainModule = linkWithLibrary(mainModule, Uclibc);
+  assert(mainModule && "unable to link with uclibc");
+
+  // more sighs, this is horrible but just a temp hack
+  //    f = mainModule->getFunction("__fputc_unlocked");
+  //    if (f) f->setName("fputc_unlocked");
+  //    f = mainModule->getFunction("__fgetc_unlocked");
+  //    if (f) f->setName("fgetc_unlocked");
+
+  Function *f2;
+  f = mainModule->getFunction("open");
+  f2 = mainModule->getFunction("__libc_open");
+  if (f2) {
+    if (f) {
+      f2->replaceAllUsesWith(f);
+      f2->eraseFromParent();
+    } else {
+      f2->setName("open");
+      assert(f2->getName() == "open");
+    }
+  }
+
+  f = mainModule->getFunction("fcntl");
+  f2 = mainModule->getFunction("__libc_fcntl");
+  if (f2) {
+    if (f) {
+      f2->replaceAllUsesWith(f);
+      f2->eraseFromParent();
+    } else {
+      f2->setName("fcntl");
+      assert(f2->getName() == "fcntl");
+    }
+  }
+
+  // XXX we need to rearchitect so this can also be used with
+  // programs externally linked with uclibc.
+
+  // We now need to swap things so that __uClibc_main is the entry
+  // point, in such a way that the arguments are passed to
+  // __uClibc_main correctly. We do this by renaming the user main
+  // and generating a stub function to call __uClibc_main. There is
+  // also an implicit cooperation in that runFunctionAsMain sets up
+  // the environment arguments to what uclibc expects (following
+  // argv), since it does not explicitly take an envp argument.
+  Function *userMainFn = mainModule->getFunction("main");
+  assert(userMainFn && "unable to get user main");
+  Function *uclibcMainFn = mainModule->getFunction("__uClibc_main");
+  assert(uclibcMainFn && "unable to get uclibc main");
+  userMainFn->setName("__user_main");
+
+  const FunctionType *ft = uclibcMainFn->getFunctionType();
+  assert(ft->getNumParams() == 7);
+
+  std::vector<const Type*> fArgs;
+  fArgs.push_back(ft->getParamType(1)); // argc
+  fArgs.push_back(ft->getParamType(2)); // argv
+  Function *stub = Function::Create(FunctionType::get(Type::getInt32Ty
+      (getGlobalContext()), fArgs, false), GlobalVariable::ExternalLinkage,
+                                    "main", mainModule);
+  BasicBlock *bb = BasicBlock::Create(getGlobalContext(), "entry", stub);
+
+  std::vector<llvm::Value*> args;
+  args.push_back(llvm::ConstantExpr::getBitCast(userMainFn,
+                                                ft->getParamType(0)));
+  args.push_back(stub->arg_begin()); // argc
+  args.push_back(++stub->arg_begin()); // argv
+  args.push_back(Constant::getNullValue(ft->getParamType(3))); // app_init
+  args.push_back(Constant::getNullValue(ft->getParamType(4))); // app_fini
+  args.push_back(Constant::getNullValue(ft->getParamType(5))); // rtld_fini
+  args.push_back(Constant::getNullValue(ft->getParamType(6))); // stack_end
+  CallInst::Create(uclibcMainFn, args.begin(), args.end(), "", bb);
+
+  new UnreachableInst(getGlobalContext(), bb);
+
+  return mainModule;
 }
 
 int main(int argc, char **argv) {
@@ -81,7 +217,7 @@ int main(int argc, char **argv) {
   assert(ReplayOut   && "can't open output file!");
 
   LogInstr *pass;
-  PassManager Passes1, Passes2, Passes3;
+  PassManager Passes1, Passes2, Passes2init, Passes3;
 
   // TODO: all optimization passes
 
@@ -99,12 +235,12 @@ int main(int argc, char **argv) {
     Passes1.add(createBitcodeWriterPass(*AnalysisOut));
 
   // record instrumentation
+  Passes2init.add(new InitInstr);
   if (!ModuleDataLayout.empty())
     Passes2.add(new TargetData(ModuleDataLayout));
 Passes2.add(createVerifierPass());
   Passes2.add(pass = new LogInstr); pass->setFuncsExportFile(FuncMapFilename);
 Passes2.add(createVerifierPass());
-  Passes2.add(new InitInstr);
   Passes2.add(createVerifierPass());
   if (OutputAssembly)
     Passes2.add(createPrintModulePass(RecordOut));
@@ -121,10 +257,20 @@ Passes2.add(createVerifierPass());
   else
     Passes3.add(createBitcodeWriterPass(*ReplayOut));
 
+  // analysis.bc
   Passes1.run(*M.get());
+
+  // clone module for replay.bc
   auto_ptr<Module> MC;
   MC.reset(CloneModule(M.get()));
+
+  // record.bc
+  Passes2init.run(*M.get());
+  if(!Uclibc.empty())
+    M.reset(linkWithUclibc(M.get()));
   Passes2.run(*M.get());
+
+  // replay.bc
   Passes3.run(*MC.get());
 
   delete AnalysisOut;
