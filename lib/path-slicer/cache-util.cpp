2c02a1394d152e52b70915aae3840f891b829d07
heming <heming@heming.(none)>
2011-12-16 18:21:59 -0500

    Add cache util for path slicer.

---
+++ lib/path-slicer/cache-util.cpp
@@ -0,0 +1,37 @@
+#include "cache-util.h"
+using namespace tern;
+
+CacheUtil::CacheUtil() {
+
+}
+
+CacheUtil::~CacheUtil() {
+
+}
+
+bool CacheUtil::in(long high1, long low1, long high2, long low2, bool 
+  &cachedResult) {
+
+  return false;
+}
+
+void CacheUtil::add(long high1, long low1, long high2, long low2, bool result) {
+
+}
+
+void CacheUtil::delete(long high1, long low1, long high2, long low2) {
+
+}
+
+bool CacheUtil::in(long high, long low, bool &cachedResult) {
+  return false;
+}
+
+void CacheUtil::add(long high, long low, bool result) {
+
+}
+
+void CacheUtil::delete(long high, long low) {
+
+}
+
