158f6a2b209d4c9899adde339fa019e111630ae9
huayang <huayang@cs.columbia.edu>
2011-12-16 15:16:29 -0500

    finished all functions hook defined in sync_func.def.h

--- dync_hook/code_gen.cpp
+++ dync_hook/code_gen.cpp
@@ -2,11 +2,49 @@
 #include <stdlib.h>
 #include <cstring>
 #include <string>
+#include <set>
 
 using namespace std;
 
 string func_pattern; 
 string void_func_pattern;
+set<string> filter;
+
+void init_filter()
+{
+  filter.clear();
+#define add(x) filter.insert(#x)
+  add(const);
+  add(void);
+  add(int);
+  add(unsigned);
+  add(pthread_mutex_t);
+  add(restrict);
+  add(pthread_mutexattr_t);
+  add(struct);
+  add(timespec);
+  add(pthread_cond_t);
+  add(sem_t);
+  add(pthread_barrier_t);
+  add(pthread_barrierattr_t);
+  add(useconds_t);
+  add(__useconds_t);
+  add(sockaddr);
+  add(size_t);
+  add(ssize_t);
+  add(msghdr);
+  add(socklen_t);
+  add(epoll_event);
+#undef add
+}
+
+inline bool goodchar(char ch)
+{
+  return ch <= 'Z' && ch >= 'A'
+    || ch <= 'z' && ch >= 'a'
+    || ch <= '9' && ch >= '0'
+    || ch == '_';
+}
 
 void replace(string &st, string pat, string tobe)
 {
@@ -71,34 +109,77 @@ void convert(FILE *fin)
 	while (true)
 	{
 		string flag = "";
-		while (flag != "STARTDEF")
+		while (flag != "STARTDEF" && flag != "START_SHORT_DEFINE")
 			if (!read_line(fin, flag)) break;
-		if (flag != "STARTDEF") break;
-		
-		string func_ret_type;
+
+    string func_ret_type;
 		string func_name;
 		string args_with_name = "";
 		string args_without_name = "";
 		string args_only_name = "";
 		string lib_path = "";
-		if (!read_line(fin, func_ret_type)) break;
-		if (!read_line(fin, func_name)) break;
-		if (!read_line(fin, lib_path)) break;
-		while (read_line(fin, flag) && flag != "SECTION")
-			args_without_name += flag;
-		while (read_line(fin, flag) && flag != "SECTION")
-			args_with_name += flag;
-		while (read_line(fin, flag) && flag != "SECTION")
-			args_only_name += flag;
-		print_func(
-			hook_cpp,
-			func_ret_type.c_str(), 
-			func_name.c_str(), 
-			args_with_name.c_str(), 
-			args_without_name.c_str(), 
-			args_only_name.c_str(), 
-			lib_path.c_str()
-		);
+		if (flag == "STARTDEF")
+    {		
+  		if (!read_line(fin, func_ret_type)) break;
+  		if (!read_line(fin, func_name)) break;
+  		if (!read_line(fin, lib_path)) break;
+  		while (read_line(fin, flag) && flag != "SECTION")
+  			args_without_name += flag;
+  		while (read_line(fin, flag) && flag != "SECTION")
+  			args_with_name += flag;
+  		while (read_line(fin, flag) && flag != "SECTION")
+  			args_only_name += flag;
+    } else
+    if (flag == "START_SHORT_DEFINE")
+    {
+  		if (!read_line(fin, lib_path)) break;
+  		if (!read_line(fin, func_ret_type)) break;
+  		if (!read_line(fin, func_name)) break;
+      string st;
+  		if (!read_line(fin, st)) break;
+      while (st[0] != '(') st = st.substr(1);
+      while (st[st.size() - 1] != ')') st = st.substr(0, st.size() - 1);
+      int len = st.size();
+      st = st.substr(1, len - 2);
+      args_with_name = st;
+      args_only_name = "";
+      args_without_name = "";
+      do {
+        string token = "";
+        while (st.size() && goodchar(st[0]))
+        {
+          token = token + st[0];
+          st = st.substr(1);
+        }
+
+        if (token.size())
+          if (filter.find(token) == filter.end())
+            args_only_name += token;
+          else
+            args_without_name += token;
+        else {
+          if (st[0] == ',')
+          {
+            args_only_name += ',';
+            args_without_name += ',';
+          } else
+            args_without_name += st[0];
+          st = st.substr(1);
+        }
+      } while (st.size());
+    } else
+      break;  		
+
+    print_func(
+  	  hook_cpp,
+    	func_ret_type.c_str(), 
+  		func_name.c_str(), 
+  		args_with_name.c_str(), 
+  		args_without_name.c_str(), 
+  		args_only_name.c_str(), 
+  		lib_path.c_str()
+  	);
+
 
     fprintf(types, "typedef %s (*__%s_funcdef)(%s);\n", 
       func_ret_type.c_str(), 
@@ -123,6 +204,7 @@ string read_file(const char *name)
 
 int main()
 {
+  init_filter();
 	func_pattern = read_file("func_template.cpp");
 	void_func_pattern = read_file("void_func_template.cpp");
 	convert(stdin);
