7345a8a874ae6fbecb2b0df37b5f596274fe0ea9
heming <heming@heming.(none)>
2012-01-10 14:59:17 -0500

    Created one testcase of one type of instruction in intra-thread slicer.

--- lib/path-slicer/macros.h
+++ lib/path-slicer/macros.h
@@ -5,8 +5,8 @@
 #define DBG1 1 /* Dbg print everything. */
 #define DBG2 2 /* Dbg print only some important events. */
 
-#define SERRS if(get_option(tern,slicer_print_debug_info)==DBG1)errs()
-#define SERRS2 if(get_option(tern,slicer_print_debug_info)==DBG2)errs()
+#define SERRS if(get_option(tern_path_slicer,print_debug_info)==DBG1)errs()
+#define SERRS2 if(get_option(tern_path_slicer,print_debug_info)==DBG2)errs()
 
 #define GETTIME(clock) gettimeofday(&clock,NULL)
 #define ADDTIME(num, clock_st, clock_end) \
@@ -14,9 +14,9 @@
 #define BEGINTIME(clock) GETTIME(clock)
 #define ENDTIME(num, clock_st, clock_end) GETTIME(clock_end);ADDTIME(num, clock_st, clock_end)
 
-#define NORMAL_SLICING (get_option(tern,slicer_do_max_slicing)==0)
-#define MAX_SLICING (get_option(tern,slicer_do_max_slicing)==1)
-#define RANGE_SLICING (get_option(tern, slicer_do_max_slicing)==2)
+#define NORMAL_SLICING (get_option(tern_path_slicer,slicing_mode)==0)
+#define MAX_SLICING (get_option(tern_path_slicer,slicing_mode)==1)
+#define RANGE_SLICING (get_option(tern_path_slicer, slicing_mode)==2)
 
 #endif
 
