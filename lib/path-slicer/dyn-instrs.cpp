8e6a9825c14648503f5d88c969ba49341573ca18
heming <heming@heming.(none)>
2012-01-18 00:34:30 -0500

    Finish core algorithm for intra-thread slicer and some other modules.

---
+++ lib/path-slicer/dyn-instrs.cpp
@@ -0,0 +1,125 @@
+#include "dyn-instrs.h"
+#include "macros.h"
+using namespace tern;
+
+DynInstr::DynInstr() {
+
+}
+
+DynInstr::~DynInstr() {
+
+}
+
+Instruction *DynInstr::getOrigInstr() {
+  return region->getOrigInstr(this);
+}
+
+DynOprd *DynInstr::getUsedOprd(unsigned index) {
+  ASSERT(index < usedOprds.size());
+  return usedOprds[index];
+}
+
+DynOprd *DynInstr::getDestOprd() {
+  return destOprd;
+}
+
+bool DynInstr::isTarget() {
+  return isTaken();
+}
+
+DynPHIInstr::DynPHIInstr() {
+
+}
+
+DynPHIInstr::~DynPHIInstr() {
+
+}
+
+void DynPHIInstr::setIncomingIndex(unsigned index) {
+  incomingIndex = index;
+}
+
+unsigned DynPHIInstr::getIncomingIndex() {
+  return incomingIndex;
+}
+
+DynBrInstr::DynBrInstr() {
+
+}
+
+DynBrInstr::~DynBrInstr() {
+
+}
+
+DynRetInstr::DynRetInstr() {
+
+}
+
+DynRetInstr::~DynRetInstr() {
+
+}
+
+void DynRetInstr::setDynCallInstr(DynInstr *dynInstr) {
+  dynCallInstr = dynInstr;
+}
+
+DynInstr *DynRetInstr::getDynCallInstr() {
+  return dynCallInstr;
+}
+
+DynCallInstr::DynCallInstr() {
+
+}
+
+DynCallInstr::~DynCallInstr() {
+
+}
+
+void DynCallInstr::setCalledFunc(llvm::Function *f) {
+  calledFunc = f;
+}
+
+llvm::Function *DynCallInstr::getCalledFunc() {
+  return calledFunc;
+}
+
+DynSpawnThreadInstr::DynSpawnThreadInstr() {
+
+}
+
+DynSpawnThreadInstr::~DynSpawnThreadInstr() {
+
+}
+
+void DynSpawnThreadInstr::setChildTid(int childTid) {
+  this->childTid = childTid;
+}
+
+int DynSpawnThreadInstr::getChildTid() {
+  return childTid;
+}
+
+DynMemInstr::DynMemInstr() {
+
+}
+
+DynMemInstr::~DynMemInstr() {
+
+}
+
+void DynMemInstr::setMemAddr(long memAddr) {
+  this->memAddr = memAddr;
+}
+
+long DynMemInstr::getMemAddr() {
+  return memAddr;
+}
+
+void DynMemInstr::setMemAddrSymbolic(bool sym) {
+  isAddrSymbolic = sym;
+}
+
+bool DynMemInstr::isMemAddrSymbolic() {
+  return isAddrSymbolic;
+}
+
---
+++ lib/path-slicer/dyn-instrs.h
@@ -0,0 +1,187 @@
+#ifndef __TERN_PATH_SLICER_DYN_INSTRS_H
+#define __TERN_PATH_SLICER_DYN_INSTRS_H
+
+#include <vector>
+#include <list>
+namespace tern {
+  class DynInstr;
+  /* Composed types of a dynamic instruction. */
+  typedef std::vector<DynInstr *> DynInstrVector;
+  typedef std::list<DynInstr *> DynInstrList;
+  typedef DynInstrList::iterator DynInstrItr;
+}
+
+#include "dyn-oprd.h"
+#include "instr-region.h"
+
+namespace tern {
+  class InstrRegion;
+  /* We must include as few as fields in this class in order to save memory. 
+    The main idea to get memory efficiency is to keep as many as information in its region.
+    Only keep fields that MUST belong to all dynamic instructions in this class. */
+  class DynInstr {
+  private:
+
+  protected:
+    /* A pointer to the region that this instr belongs to, the region contains as many as shared info
+      of instructions within this region, such as thread id and vector clock. */
+    InstrRegion *region;
+
+    /* Use a separate pointer to store destination operand. This would not cost more memory
+    since most LLVM instructions have destination operand. */
+    DynOprd *destOprd;
+
+    /* All used operands of this instruction, including function call arguments. */
+    std::vector<DynOprd * > usedOprds;
+
+    /* The total order of execution index of this dynamic instr.
+        Question: does xtern still have this feature? */
+    size_t index;
+
+    /* In normal and max slicing mode, this is the normal and max sliced calling context, respectively. */
+    std::vector<int> *callingCtx;
+
+    /* This is only valid in simplified slicing mode. 
+      For each dynamic instruction, there should be only one callstack in simplified bc, right?
+      TODO: MUST THINK OF A NICE WAY TO ELIMINATE THIS MEMORY TOO. */
+    std::vector<int> *simCallingCtx;
+
+  public:
+    DynInstr();
+    ~DynInstr();
+
+    /* TBD */
+    int getTid();
+
+    /* TBD */
+    size_t getIndex();
+
+    /* TBD */
+    void setCallingCtx(std::vector<int> &ctx);
+
+    /* TBD */
+    std::vector<int> *getCallingCtx();
+
+    /* TBD */
+    void setSimCallingCtx(std::vector<int> &ctx);
+
+    /* TBD */
+    std::vector<int> *getSimCallingCtx();
+
+    /* TBD */
+    int getOrigInstrId();
+
+    /* TBD */
+    int getMxInstrId();
+
+    /* TBD */
+    std::set<int> *getSimInstrId();
+
+    /* TBD */
+    void setTaken(bool isTaken, std::string reason = NOT_TAKEN_INSTR);
+
+    /* TBD */
+    bool isTaken();
+
+    /* Whether an instruction is a slicing target already, currently it is the same as taken. */
+    bool isTarget();
+
+    /* TBD */
+    llvm::Instruction *getOrigInstr();
+
+    /* TBD */
+    DynOprd *getUsedOprd(unsigned index);
+
+    /* TBD */
+    DynOprd *getDestOprd();
+  };
+
+  class DynPHIInstr: public DynInstr {
+  private:
+    unsigned incomingIndex;
+
+  protected:
+    
+  public:
+    DynPHIInstr();
+    ~DynPHIInstr();
+    void setIncomingIndex(unsigned index);
+    unsigned getIncomingIndex();
+    
+  };
+
+  class DynBrInstr: public DynInstr {
+  private:
+
+  protected:
+
+  public:
+    DynBrInstr();
+    ~DynBrInstr();
+    
+  };
+
+  class DynRetInstr: public DynInstr {
+  private:
+    /* The dynamic call instruction corresponds to this return instruction. */
+    DynInstr *dynCallInstr;
+
+  protected:
+
+  public:
+    DynRetInstr();
+    ~DynRetInstr();
+    void setDynCallInstr(DynInstr *dynInstr);
+    DynInstr *getDynCallInstr();
+    
+  };
+
+  class DynCallInstr: public DynInstr {
+  private:
+
+  protected:
+    llvm::Function *calledFunc;
+
+  public:
+    DynCallInstr();
+    ~DynCallInstr();
+    void setCalledFunc(llvm::Function *f);
+    llvm::Function *getCalledFunc();
+    
+  };
+
+  class DynSpawnThreadInstr: public DynCallInstr {
+  private:
+    int childTid;
+
+  protected:
+    
+
+  public:
+    DynSpawnThreadInstr();
+    ~DynSpawnThreadInstr();
+    void setChildTid(int childTid);
+    int getChildTid();
+  };
+
+  class DynMemInstr: public DynInstr {
+  private:
+    /* Loaded or stored memory address from the pointer operand. */
+    long memAddr;
+    bool isAddrSymbolic;
+
+  protected:
+
+  public:
+    DynMemInstr();
+    ~DynMemInstr();
+    void setMemAddr(long memAddr);
+    long getMemAddr();
+    void setMemAddrSymbolic(bool sym);
+    bool isMemAddrSymbolic();
+  };
+}
+
+#endif
+
+
