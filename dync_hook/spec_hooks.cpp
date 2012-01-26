90146bf027b5a27caae53592d9507e55f26256bf
Junfeng Yang <junfeng@cs.columbia.edu>
2012-01-26 13:18:02 -0500

    bug fix: __tern_prog_end is not called for the main thread
    reason: we used to hook __libc_start_main() and call __tern_prog_begin and
    __tern_prog_end before and after user's main function.  However, if user's
    main function calls exit(), the __tern_prog_end call will be skipped.
    
    fix: hook the init_func and rtld_fini_func arguments to __libc_start_main
    to call __tern_prog_begin and __tern_prog_end instead. rtld_fini_func will
    be called even if user's main function calls exit().

--- dync_hook/spec_hooks.cpp
+++ dync_hook/spec_hooks.cpp
@@ -1,7 +1,14 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <string.h>
+
 #ifdef __SPEC_HOOK___libc_start_main
 
 extern "C" void __tern_prog_begin(void);  //  lib/runtime/helper.cpp
 extern "C" void __tern_prog_end(void); //  lib/runtime/helper.cpp
+//extern "C" void tern_exit(unsigned ins, int status); //  lib/runtime/runtime.cpp
 
 typedef int (*main_type)(int, char**, char**);
 
@@ -11,60 +18,87 @@ struct arg_type
   int (*main_func) (int, char **, char **);
 };
 
+main_type saved_init_func = NULL;
+void tern_init_func(int argc, char **argv, char **env){
+  if(saved_init_func)
+    saved_init_func(argc, argv, env);
+  __tern_prog_begin();
+}
+
+typedef void (*fini_type)(void*);
+fini_type saved_fini_func = NULL;
+void tern_fini_func(void* status) {
+  // note that fprintf may not print out anything here because (1) stderr
+  // may be redirected and (2) we're close to program exit and the libc
+  // data structures may already be cleared
+  fprintf(stderr, "%04d: __tern_prog_end() called.\n", (int) pthread_self());
+  __tern_prog_end();
+  if(saved_fini_func)
+    saved_fini_func(status);
+}
+
 extern "C" int my_main(int argc, char **pt, char **aa)
 {
   int ret;
   arg_type *args = (arg_type*)pt;
-  __tern_prog_begin();
-	fprintf(stderr, "%04d: __libc_start_main() called.\n", (int) pthread_self());
+  fprintf(stderr, "%04d: __libc_start_main() called.\n", (int) pthread_self());
   ret = args->main_func(argc, args->argv, aa);
-	fprintf(stderr, "%04d: __tern_prog_end() called.\n", (int) pthread_self());
   fflush(stderr);
-  __tern_prog_end();
   return ret;
 }
 
 extern "C" int __libc_start_main(
-  void *func_ptr, 
-  int argc, 
-  char* argv[], 
-  void *init_func,
-  void *fini_func, 
+  void *func_ptr,
+  int argc,
+  char* argv[],
+  void (*init_func)(void),
+  void (*fini_func)(void),
+  void (*rtld_fini_func)(void),
   void *stack_end)
 {
-	typedef int (*orig_func_type)(void *, int, char *[], void*, void*, void*);
-  
-	orig_func_type orig_func;
+  typedef void (*fnptr_type)(void);
+  typedef int (*orig_func_type)(void *, int, char *[], fnptr_type,
+                                fnptr_type, fnptr_type, void*);
+  orig_func_type orig_func;
   arg_type args;
 
-	void * handle;
-	int ret;
+  void * handle;
+  int ret;
 
-	if(!(handle=dlopen("/lib/x86_64-linux-gnu/libc.so.6", RTLD_LAZY))) {
-		puts("dlopen error");
-		abort();
-	}
+  if(!(handle=dlopen("/lib/x86_64-linux-gnu/libc.so.6", RTLD_LAZY))) {
+    puts("dlopen error");
+    abort();
+  }
 
-	orig_func = (orig_func_type) dlsym(handle, "__libc_start_main");
+  orig_func = (orig_func_type) dlsym(handle, "__libc_start_main");
 
-	if(dlerror()) {
-		puts("dlerror");
-		abort();
-	}
+  if(dlerror()) {
+    puts("dlerror");
+    abort();
+  }
 
-	dlclose(handle);
+  dlclose(handle);
 
 #ifdef __USE_TERN_RUNTIME
 #ifdef PRINT_DEBUG
-	fprintf(stderr, "%04d: __libc_start_main is hooked.\n", (int) pthread_self());
+  fprintf(stderr, "%04d: __libc_start_main is hooked.\n", (int) pthread_self());
 #endif
   //fprintf(stderr, "%04d: __tern_prog_begin() called.\n", (int) pthread_self());
   args.argv = argv;
   args.main_func = (main_type)func_ptr;
-  ret = orig_func((void*)my_main, argc, (char**)(&args), init_func, fini_func, stack_end);
+  saved_init_func = (main_type)init_func;
+  // note that we don't hook fini_func because it appears that fini_func
+  // is only called for statically linked libc, whereas rtld_fini_func is
+  // called regardless
+  saved_fini_func = (fini_type)rtld_fini_func;
+
+  ret = orig_func((void*)my_main, argc, (char**)(&args),
+                  (fnptr_type)tern_init_func, (fnptr_type)fini_func,
+                  (fnptr_type)tern_fini_func, stack_end);
   return ret;
 #endif
-	ret = orig_func(func_ptr, argc, argv, init_func, fini_func, stack_end);
+  ret = orig_func(func_ptr, argc, argv, init_func, fini_func,
+                  rtld_fini_func, stack_end);
 
 	return ret;
 }
