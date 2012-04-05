2af4c385413da91606ee384e7c56c57d88cb1fcc
Yi-Hong Lin <yl2790@columbia.edu>
2012-04-04 22:01:48 -0400

    make each dlsym been called exactly once
    1. make the functor be a static variable

--- dync_hook/func_template.cpp
+++ dync_hook/func_template.cpp
@@ -2,26 +2,7 @@
 extern "C" FUNC_RET_TYPE FUNC_NAME(ARGS_WITH_NAME){
   typedef FUNC_RET_TYPE (*orig_func_type)(ARGS_WITHOUT_NAME);
 
-  orig_func_type orig_func;
-
-  void * handle;
   FUNC_RET_TYPE ret;
-
-  if(!(handle=dlopen("LIB_PATH", RTLD_LAZY))) {
-    perror("dlopen");
-    puts("here dlopen");
-    abort();
-  }
-
-  orig_func = (orig_func_type) dlsym(handle, "FUNC_NAME");
-
-  if(dlerror()) {
-    perror("dlsym");
-    puts("here dlsym");
-    abort();
-  }
-
-  dlclose(handle);
 
 #ifdef __USE_TERN_RUNTIME
   if (Space::isApp() && options::DMT) {
@@ -53,6 +34,27 @@ extern "C" FUNC_RET_TYPE FUNC_NAME(ARGS_WITH_NAME){
   fprintf(stdout, "%04d: FUNC_NAME is called.\n", (int) pthread_self());
   fflush(stdout);
 #endif
+
+  static orig_func_type orig_func = NULL;
+  if (!orig_func) {
+    void * handle;
+    if(!(handle=dlopen("LIB_PATH", RTLD_LAZY))) {
+      perror("dlopen");
+      puts("here dlopen");
+      abort();
+    }
+
+    orig_func = (orig_func_type) dlsym(handle, "FUNC_NAME");
+
+    if(dlerror()) {
+      perror("dlsym");
+      puts("here dlsym");
+      abort();
+    }
+
+    dlclose(handle);
+  }
+
   ret = orig_func(ARGS_ONLY_NAME);
 
   return ret;
--- dync_hook/void_func_template.cpp
+++ dync_hook/void_func_template.cpp
@@ -2,26 +2,6 @@
 extern "C" void FUNC_NAME(ARGS_WITH_NAME){
   typedef int (*orig_func_type)(ARGS_WITHOUT_NAME);
 
-  orig_func_type orig_func;
-
-  void * handle;
-
-  if(!(handle=dlopen("LIB_PATH", RTLD_LAZY))) {
-    perror("dlopen");
-    puts("here dlopen");
-    abort();
-  }
-
-  orig_func = (orig_func_type) dlsym(handle, "FUNC_NAME");
-
-  if(dlerror()) {
-    perror("dlsym");
-    puts("here dlsym");
-    abort();
-  }
-
-  dlclose(handle);
-
 #ifdef __USE_TERN_RUNTIME
   if (Space::isApp() && options::DMT) {
 
@@ -45,6 +25,27 @@ extern "C" void FUNC_NAME(ARGS_WITH_NAME){
 #ifdef PRINT_DEBUG
   fprintf(stdout, "%04d: FUNC_NAME is hooked.\n", (int) pthread_self());
 #endif
+
+  static orig_func_type orig_func = NULL;
+  if ( !orig_func ) {
+    void * handle;
+
+    if(!(handle=dlopen("LIB_PATH", RTLD_LAZY))) {
+      perror("dlopen");
+      puts("here dlopen");
+      abort();
+    }
+
+    orig_func = (orig_func_type) dlsym(handle, "FUNC_NAME");
+
+    if(dlerror()) {
+      perror("dlsym");
+      puts("here dlsym");
+      abort();
+    }
+
+    dlclose(handle);
+  }
 
   orig_func(ARGS_ONLY_NAME);
 }
