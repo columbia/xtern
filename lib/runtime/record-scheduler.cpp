69c4a162c371a6944222009c11efa473b6f19a85
Henry Hu <henry.hu.sh@gmail.com>
2012-04-22 23:42:57 -0400

    fix conflicts

--- lib/runtime/record-scheduler.cpp
+++ lib/runtime/record-scheduler.cpp
@@ -10,11 +10,8 @@
 #include <cstdio>
 #include <stdlib.h>
 #include <cstring>
-<<<<<<< HEAD
 #include <algorithm>
-=======
 #include <unistd.h>
->>>>>>> 367e9100fb77b07302114c5fd67fcf6084caf182
 #include "tern/options.h"
 
 using namespace std;
