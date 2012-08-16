fe2ae6172fb53f7af0a54b7f03723b7699617a71
huayang <huayang@cs.columbia.edu>
2012-08-16 15:49:20 -0400

    add support to network operations in logparser.py.

--- dync_hook/logparser.py
+++ dync_hook/logparser.py
@@ -68,6 +68,8 @@ def readFromLogFile(filename, pid, tid):
   return ret
 
 HANDLED_OPNAMES = {
+  'tern_thread_begin'             : 'BEGIN',
+  'tern_thread_end'               : 'END',
   'pthread_create'                : 'P_C',
   'pthread_join'                  : 'P_J',
   'pthread_mutex_lock'            : 'M_L',
@@ -76,8 +78,127 @@ HANDLED_OPNAMES = {
   'pthread_barrier_init'          : 'B_I',
   'pthread_barrier_wait_first'    : 'B_W_F',
   'pthread_barrier_wait_second'   : 'B_W_S',
+
+#   network functions
+  'accept_first'                  : 'ACC_F',
+  'accept_second'                 : 'ACC_S',
+  'connect_first'                 : 'CONN_F',
+  'connect_second'                : 'CONN_S',
+  'write'                         : 'WRITE',
+  'read_first'                    : 'READ_F',
+  'read_second'                   : 'READ_S',
+#  'recv_first'                    : 'RECV_F',
+#  'recv_second'                   : 'RECV_S',
+#  'select_first'                  : 'SELE_F',
+#  'select_second'                 : 'SELE_S',
+#  'epoll_wait_first'              : 'EPOL_F',
+#  'epoll_wait_second'             : 'EPOL_S',
+#  'sigwait_first'                 : 'SIGW_F',
+#  'sigwait_second'                : 'SIGW_S',
   'unknown'                       : 'unknown'
   }
+
+def findConnDeps(ops):
+  """
+  args format:
+    ACC_F:      (FD) (listen_port) (local_port)
+    ACC_S:      (FD) (listen_port) (local_port)
+    CONN_F:     (FD) (listen_port) (local_port) (ret)
+    CONN_S:     (FD) (listen_port) (local_port) (ret)
+    WRITE:      (sig) (FD) (size)
+    READ_F:     (sig) (FD) (size)
+    READ_S:     (sig) (FD) (size)
+
+    the function doesn't handle FD reuse. 
+  """
+  MUTEX_OPS = ['ACC_F', 'ACC_S', 'CONN_F', 'CONN_S', 'WRITE', 'READ_F', 'READ_S']
+  deps = []
+  portstat = dict()
+  remote = dict()    # localFD -> remoteFD
+  sendqueue = dict()      # FD -> sending msg queue
+
+  for op in ops:
+    opname = op['op']
+    if (opname not in MUTEX_OPS):
+      continue
+    args = op['args']
+    opid = op['id']
+
+    if (opname == 'ACC_F'):
+      (fd, listen_port, local_port) = args.split()
+      fd = op['pid'] + '_' + fd
+      portstat.setdefault(listen_port, dict())
+      portstat[listen_port].setdefault('accept', [])
+      portstat[listen_port]['accept'].append(op)
+
+    if (opname == 'CONN_F'):
+      (fd, listen_port, local_port, ret) = args.split()
+      fd = op['pid'] + '_' + fd
+      portstat.setdefault(listen_port, dict())
+      portstat[listen_port].setdefault('connect', [])
+      portstat[listen_port]['connect'].append(op)
+
+    if (opname == 'ACC_S'):
+      (fd, listen_port, local_port) = args.split()
+      fd = op['pid'] + '_' + fd
+      # here we're assuming that the first connection operation succeeds
+      connstat = portstat[listen_port]['connect']
+      assert len(connstat) > 0
+      conn_op = connstat[0]
+      remote_fd = conn_op['args'].split()[0]
+      remote_fd = conn_op['pid'] + '_' + remote_fd
+      remote[fd] = remote_fd
+      deps.append((conn_op['id'], opid, 
+        'listenPort = %s, accFD = %s, connFD = %s' % (listen_port, fd, remote_fd)))
+      portstat[listen_port]['connect'] = connstat[1:]
+
+    if (opname == 'CONN_S'):
+      (fd, listen_port, local_port, ret) = args.split()
+      fd = op['pid'] + '_' + fd
+      # here we're assuming that the first connection operation succeeds
+      accstat = portstat[listen_port]['accept']
+      assert len(accstat) > 0
+      acc_op = accstat[0]
+      remote_fd = acc_op['args'].split()[0]
+      remote_fd = acc_op['pid'] + '_' + remote_fd
+      remote[fd] = remote_fd
+      deps.append((acc_op['id'], opid, 
+        'listenPort = %s, accFD = %s, connFD = %s' % (listen_port, remote_fd, fd)))
+      portstat[listen_port]['accept'] = accstat[1:]
+
+    if (opname == 'WRITE'):
+      (sig, fd, size) = args.split()
+      size = int(size, 16)
+      fd = op['pid'] + '_' + fd
+      if (fd not in remote):
+        continue
+      sendqueue.setdefault(fd, [])
+      sendqueue[fd].append([size, opid])
+
+    if (opname == 'READ_F'):
+      pass
+
+    if (opname == 'READ_S'):
+      (sig, fd, size) = args.split()
+      size = int(size, 16)
+
+      fd = op['pid'] + '_' + fd
+      if (fd not in remote):
+        continue
+
+      remote_fd = remote[fd] 
+      while (size > 0):
+        sq = sendqueue[remote_fd]
+        assert remote_fd in sendqueue and len(sq) > 0
+        msg = sq[0]
+        recvbytes = min(msg[0], size)
+        deps.append((msg[1], opid, 'recv %d bytes' % (recvbytes)))
+        size = size - recvbytes
+        msg[0] = msg[0] - recvbytes
+        if (msg[0] == 0):
+          sendqueue[remote_fd] = sq[1:]
+
+  return deps
 
 def findMutexDeps(ops):
   """
@@ -104,6 +225,61 @@ def findMutexDeps(ops):
     lastAccessed[mutex] = opid
 
   return deps
+
+def findThreadDeps(ops):
+  """
+  args format:
+    BEGIN:    (pthread_id)
+    END:      (pthread_id)
+    P_C:      (child_thread_id) (return value)
+    P_J:      (child_thread_id)
+  """
+
+  TARGET_OPS = ['BEGIN', 'END', 'P_C', 'P_J']
+  createOps = dict()
+  endOps = dict()
+  deps = []
+
+  for op in ops:
+    opname = op['op']
+    if (opname not in TARGET_OPS):
+      continue
+
+    args = op['args']
+    opid = op['id']
+
+    if (opname == 'BEGIN'):
+      pthread_id = args.split()[0]
+      pthread_id = op['pid'] + '_' + pthread_id
+      if (pthread_id in createOps):
+        deps.append((createOps[pthread_id], opid, pthread_id))
+        del createOps[pthread_id]
+
+    if (opname == 'P_C'):
+      (pthread_id, ret) = args.split()
+      pthread_id = op['pid'] + '_' + pthread_id
+      createOps[pthread_id] = op['id']
+
+    if (opname == 'END'):
+      pthread_id = args.split()[0]
+      pthread_id = op['pid'] + '_' + pthread_id
+      endOps[pthread_id] = op['id']
+      
+    if (opname == 'P_J'):
+      pthread_id = args.split()[0]
+      pthread_id = op['pid'] + '_' + pthread_id
+      if (pthread_id in endOps):
+        deps.append((endOps[pthread_id], opid, pthread_id))
+        del endOps[pthread_id]
+
+
+  return deps
+
+def condenseTurns(ops):
+  turn = 0
+  for op in ops:
+    op['turn'] = str(turn)
+    turn = turn + 1
 
 def findBarrierDeps(ops):
   """
@@ -195,6 +371,8 @@ def findDeps(ops):
 
   ret += findMutexDeps(ops)
   ret += findBarrierDeps(ops)
+  ret += findThreadDeps(ops)
+  ret += findConnDeps(ops)
 
   if False:
     for x in ret:
@@ -206,8 +384,16 @@ def findDeps(ops):
 
   return ret
 
+def filterThreads(ops, tids):
+  ret = []
+  for op in ops:
+    if not op['tid'] in tids:
+      ret.append(op)
+  return ret
+
 def filterUnknownOps(ops):
   unhandled_opnames = set()
+  ret = []
   for op in ops:
     opname = op['op']
     if (opname not in HANDLED_OPNAMES):
@@ -217,12 +403,15 @@ def filterUnknownOps(ops):
     else:
       # otherwise, we remap the opname to a shortened standard opname
       op['op'] = HANDLED_OPNAMES[opname]
+      ret.append(op)
 
   if (len(unhandled_opnames)):
     print >>sys.stderr, '%d unhandled opname(s)' % len(unhandled_opnames)
     print >>sys.stderr, '    ', unhandled_opnames
 
-def parseLog(dirname, nopid = False):
+  return ret
+
+def parseLog(dirname, nopid = False, filterIdle = True):
   filenames = get_logfiles(dirname)
   print >>sys.stderr, 'filenames', filenames
 
@@ -253,13 +442,16 @@ def parseLog(dirname, nopid = False):
   for i in range(0, len(ops)):
     ops[i]['id'] = i
   
-  filterUnknownOps(ops)
+  ops = filterUnknownOps(ops)
+  ops = filterThreads(ops, ['1'])
+  condenseTurns(ops)
   deps = findDeps(ops)
   printDepGraph(ops, deps)
 
 if __name__ == '__main__':
   args = sys.argv
   nopid = False
+  filterIdleSleep = True
   for arg in args:
     if arg == "nopid":
       nopid = True
