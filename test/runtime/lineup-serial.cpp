a47ad29b97f37635f84837a4a2f8af4a9aa003b3
Heming Cui <heming@heming.(none)>
2012-12-22 23:51:09 -0500

    Added the first testcase showing that the lineup primitive could solve the serialization problem in xtern.

---
+++ test/runtime/lineup-serial.cpp
@@ -0,0 +1,99 @@
+// RUN: %srcroot/test/runtime/run-scheduler-test.py %s -gxx "%gxx" -llvmgcc "%llvmgcc" -projbindir "%projbindir" -ternruntime "%ternruntime" -ternannotlib "%ternannotlib"  -ternbcruntime "%ternbcruntime"
+
+// Lineup the computation blocks which are serialized by the xtern RR 
+// scheduler. The overhead with xtern now is very little after adding this lineup.
+
+#include <stdio.h>
+#include "tern/user.h"
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+pthread_mutex_t mu = PTHREAD_MUTEX_INITIALIZER;
+const long workload = 1e8;
+
+void loop(const char *tag) {
+  const int loopCount = 10;
+  for (int i = 0; i < loopCount; i++) { 
+    pthread_mutex_lock(&mu);
+    pthread_mutex_unlock(&mu);
+
+    tern_lineup_start(0);
+    tern_lineup_end(0);   
+
+    long sum = 0;
+    for (long j = 0; j < workload; j++)
+      sum += j*j;
+
+    pthread_mutex_lock(&mu);
+    printf("%s sum: %ld\n", tag, sum);
+    pthread_mutex_unlock(&mu);
+  }
+}
+
+void* thread_func(void* arg) {
+  loop((const char *)arg);
+}
+
+int main(int argc, char *argv[], char* env[]) {
+  int ret;
+  const int nthreads = 4;
+  pthread_t th[nthreads];
+  const char *tags[nthreads] = {"T0", "T1", "T2", "T3"};
+
+  tern_lineup_init(0, nthreads, 20);
+
+  for (int i = 0; i < nthreads; i++) {
+    ret  = pthread_create(&th[i], NULL, thread_func, (void *)tags[i]);
+    assert(!ret && "pthread_create() failed!");
+  }
+  for (int i = 0; i < nthreads; i++)
+    pthread_join(th[i], NULL);
+
+  return 0;
+}
+
+// CHECK: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+// CHECK-NEXT: T0 sum: 662921401752298880
+// CHECK-NEXT: T1 sum: 662921401752298880
+// CHECK-NEXT: T2 sum: 662921401752298880
+// CHECK-NEXT: T3 sum: 662921401752298880
+
