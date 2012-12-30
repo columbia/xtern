e3b3491e5651467e2d27ae9ca73d113174f9174e
Heming Cui <heming@heming.(none)>
2012-12-30 02:19:56 -0500

    Nits to the imagick xtern annotation. Now the overhead for the convert utility is < 10%pico diff.patch

--- lib/runtime/record-runtime.cpp
+++ lib/runtime/record-runtime.cpp
@@ -1197,14 +1197,13 @@ template <typename _S>
 void RecorderRT<_S>::lineupInit(long opaque_type, unsigned count, unsigned timeout_turns) {
   unsigned ins = opaque_type;
   SCHED_TIMER_START;
-  fprintf(stderr, "lineupInit opaque_type ENTER %p, count %u, timeout %u\n", (void *)opaque_type, count, timeout_turns);
+  fprintf(stderr, "lineupInit opaque_type %p, count %u, timeout %u\n", (void *)opaque_type, count, timeout_turns);
   assert(refcnt_bars.find(opaque_type) == refcnt_bars.end() && "refcnt barrier already initialized!");
   refcnt_bars[opaque_type].count = count;
   refcnt_bars[opaque_type].nactive = 0;
   refcnt_bars[opaque_type].timeout = timeout_turns;
   refcnt_bars[opaque_type].setArriving();
   SCHED_TIMER_END(syncfunc::tern_lineup_init, (uint64_t)opaque_type, (uint64_t) count, (uint64_t) timeout_turns);
-  fprintf(stderr, "lineupInit opaque_type LEAVE %p, count %u, timeout %u\n", (void *)opaque_type, count, timeout_turns);
 }
 
 template <typename _S>
