df3a454dd22722e2f2b50378f4cd32b976642c2d
Yi-Hong Lin <yl2790@columbia.edu>
2013-01-04 13:33:37 -0500

    eval.py implementation
    1. get default options from default.options
    2. resolve path of config files
    3. read config file

--- eval/eval.py
+++ eval/eval.py
@@ -10,25 +10,68 @@ import coloroutput
 import re
 
 def getXternDefaultOptions():
+    default = {}
     try:
         with open(XTERN_ROOT+'/default.options') as f:
             for line in f:
                 line = line.partition('#')[0].rstrip()
                 if not line:
                     continue
+                s1, s2 = [p.strip() for p in line.split("=")]
+                if not s1 or not s2:
+                    logging.warning(
+                        'cannot get default value from "%s" (ignored)' % line)
+                    continue
+                default[s1] = s2
+                #print ', '.join(line_segs)
                 #print line
                 #if re.search('(?#...)', line) is None:
                 #    sys.stdout.write(line)
     except IOError as e:
         logging.error("There is no 'default.options' file")
         sys.exit(1)
+    logging.debug("default.options : ")
+    for key in default:
+        logging.debug("\t{0} = '{1}'".format(key,default[key]))
+    return default
     
+def getFullPath(config_file):
+    try:
+        with open(config_file) as f: pass
+    except IOError as e:
+        if config_file == 'xtern.cfg':
+            logging.warning("'xtern.cfg' does not exist in current directory"
+                    + ", use default one in XTERN_ROOT/eval")
+            return os.path.abspath(XTERN_ROOT + "/eval/xtern.cfg")
+        else:
+            logging.warning("'%s' does not exist" % config_file)
+            return None
+    return os.path.abspath(config_file)
+
+def readConfigFile(config_file):
+    try:
+        ret = config.read(config_file)
+    except ConfigParser.MissingSectionHeaderError as e:
+        logging.error(str(e))
+    except ConfigParser.ParsingError as e:
+        logging.error(str(e))
+    except ConfigParser.Error as e:
+        logging.critical("strange error? " + str(e))
+    else:
+        if ret:
+            return config
+
 
 if __name__ == "__main__":
     # setting log format
     logger = logging.getLogger()
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s",
+                                  "%Y%b%d-%H:%M:%S")
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
     logger.setLevel(logging.DEBUG)
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # parse input arguments
     parser = argparse.ArgumentParser(
@@ -56,11 +99,21 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # get default xtern options
-    getXternDefaultOptions()
+    default_options = getXternDefaultOptions()
+    # config parser
+    config = ConfigParser.ConfigParser( {"REPEATS":100,
+                                       "INPUTS": ""} )
 
-    # process the config files
     for config_file in args.filename:
         logging.info("processing '" + config_file + "'")
+        # get absolute path of config file
+        full_path = getFullPath(config_file)
+
+        if not readConfigFile(full_path):
+            logging.warning("skip " + full_path)
+            continue
+
+        # generate running directory
+        
         
     #print os.getcwd()
-    #config = ConfigParser.ConfigParser()
