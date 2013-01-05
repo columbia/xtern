0dae4d33ccc7c86be943ca35b3e4afb6ba3be312
Yi-Hong Lin <yl2790@columbia.edu>
2013-01-04 21:26:50 -0500

    backup commit
    1. generate local.options
    2. check options and extract executible filename

--- eval/eval.py
+++ eval/eval.py
@@ -23,10 +23,6 @@ def getXternDefaultOptions():
                         'cannot get default value from "%s" (ignored)' % line)
                     continue
                 default[s1] = s2
-                #print ', '.join(line_segs)
-                #print line
-                #if re.search('(?#...)', line) is None:
-                #    sys.stdout.write(line)
     except IOError as e:
         logging.error("There is no 'default.options' file")
         sys.exit(1)
@@ -35,7 +31,7 @@ def getXternDefaultOptions():
         logging.debug("\t{0} = '{1}'".format(key,default[key]))
     return default
     
-def getFullPath(config_file):
+def getConfigFullPath(config_file):
     try:
         with open(config_file) as f: pass
     except IOError as e:
@@ -50,7 +46,9 @@ def getFullPath(config_file):
 
 def readConfigFile(config_file):
     try:
-        ret = config.read(config_file)
+        newConfig = ConfigParser.ConfigParser( {"REPEATS":100,
+                                       "INPUTS": ""} )
+        ret = newConfig.read(config_file)
     except ConfigParser.MissingSectionHeaderError as e:
         logging.error(str(e))
     except ConfigParser.ParsingError as e:
@@ -59,8 +57,84 @@ def readConfigFile(config_file):
         logging.critical("strange error? " + str(e))
     else:
         if ret:
-            return config
+            return newConfig
 
+def getGitInfo():
+    import commands
+    git_show = 'cd '+XTERN_ROOT+' && git show '
+    githash = commands.getoutput(git_show+'| head -1 | sed -e "s/commit //"')
+    git_diff = 'cd '+XTERN_ROOT+' && git diff --quiet'
+    gitstatus = commands.getoutput('if ! ('+
+                                   git_diff+
+                                   '); then echo "dirty"; fi')
+    commit_date = commands.getoutput( git_show+
+            '| head -3 | grep "Date:" | sed -e "s/Date:[ \t]*//"' )
+    date_tz  = re.compile(r'^.* ([+-]\d\d\d\d)$').match(commit_date).group(1)
+    date_fmt = ('%%a %%b %%d %%H:%%M:%%S %%Y %s') % date_tz
+    import datetime
+    gitcommitdate = str(datetime.datetime.strptime(commit_date, date_fmt))
+    logging.debug( "git 6 digits hash code: " + githash[0:6] )
+    logging.debug( "git reposotory status: " + gitstatus)
+    logging.debug( "git commit date: ")
+    return [githash[0:6], gitstatus, gitcommitdate]
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # for newer python version; can specify flags
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            logging.warning("%s already exists")
+            pass
+        else: raise
+
+def genRunDir(config_file, git_info):
+    from os.path import basename
+    config_name = os.path.splitext( basename(config_file) )[0]
+    from time import strftime
+    dir_name = config_name + strftime("%Y%b%d_%H%M%S") + '_' + git_info[0] + '_' + git_info[1]
+    mkdir_p(dir_name)
+    logging.debug("creating %s" % dir_name)
+    return os.path.abspath(dir_name)
+
+def extract_apps_exec(bench):
+    bench = bench.partition('"')[0].partition("'")[0]
+    apps = bench.split()
+    if apps.__len__() < 1:
+        raise Exception("cannot parse executible file name")
+    elif apps.__len__() == 1:
+        return apps[0], os.path.abspath(APPS + '/' + apps[0] + '/' + apps[0])
+    else:
+        return apps[0], os.path.abspath(APPS + '/' + apps[0] + '/' + apps[1])
+
+def generate_local_options(config, bench):
+    config_options = config.options(bench)
+    output = ""
+    for option in default_options:
+        if option in config_options:
+            entry = option + ' = ' + config.get(bench, option)
+            logging.info(entry)
+        else:
+            entry = option + ' = ' + default_options[option]
+            #logging.debug(entry)
+        output += '%s\n' % entry
+    with open("local.options", "w") as option_file:
+        option_file.write(output)
+
+def processBench(config, bench):
+    logging.debug("processing: " + bench)
+    apps_name, exec_file = extract_apps_exec(bench)
+    logging.debug("app = %s" % apps_name)
+    logging.debug("executible file = %s" % exec_file)
+    segs = re.sub(r'(\")|(\.)|/|\'', '', bench).split()
+    dir_name =  '_'.join(segs)
+    
+    mkdir_p(dir_name)
+    os.chdir(dir_name)
+    # generate local options
+
+
+    generate_local_options(config, bench)
+    os.chdir("..")
 
 if __name__ == "__main__":
     # setting log format
@@ -97,23 +171,37 @@ if __name__ == "__main__":
     except KeyError as e:
         logging.error("Please set the environment variable " + str(e))
         sys.exit(1)
+    APPS = os.path.abspath(XTERN_ROOT + "/apps/")
+    XTERN_PRELOAD = "LD_PRELOAD=%s/dync_hook/interpose.so" % XTERN_ROOT
+    RAND_PRELOAD = "LDPRELOAD=%s/eval/rand-intercept/rand-intercept.so"
 
     # get default xtern options
     default_options = getXternDefaultOptions()
-    # config parser
-    config = ConfigParser.ConfigParser( {"REPEATS":100,
-                                       "INPUTS": ""} )
+    git_info = getGitInfo()
+    root_dir = os.getcwd()
 
     for config_file in args.filename:
         logging.info("processing '" + config_file + "'")
         # get absolute path of config file
-        full_path = getFullPath(config_file)
+        full_path = getConfigFullPath(config_file)
 
-        if not readConfigFile(full_path):
+        # config parser  
+        local_config = readConfigFile(full_path)
+        if not local_config:
             logging.warning("skip " + full_path)
             continue
 
         # generate running directory
+        run_dir = genRunDir(full_path, git_info)
+        if not run_dir:
+            continue
+        os.chdir(run_dir)
         
-        
-    #print os.getcwd()
+        benchmarks = local_config.sections()
+        for benchmark in benchmarks:
+            if benchmark == "default" or benchmark == "example":
+                continue
+            processBench(local_config, benchmark)
+
+        os.chdir(root_dir)
+       
