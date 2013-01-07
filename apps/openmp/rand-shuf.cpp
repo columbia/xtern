bb91f21d91145160d8f69c6532d581400ec683ea
Heming Cui <heming@heming.(none)>
2013-01-07 18:33:20 -0500

    Added max # of threads to a benchmark.

--- apps/openmp/rand-shuf.cpp
+++ apps/openmp/rand-shuf.cpp
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <omp.h>
 
 int main(int argc, char *argv[]) {
+  fprintf(stderr, "omp num threads %d\n", omp_get_max_threads());
   std::vector<int> vi(1000*1000*100);
   std::random_shuffle(vi.begin(), vi.end());
   return 0;
