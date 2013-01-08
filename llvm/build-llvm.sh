70b44391d98ec10b64c77ef6c1aea3f55de0393f
Heming Cui <heming@heming.(none)>
2013-01-08 17:32:01 -0500

    Modified .gitmodules and add username

--- llvm/build-llvm.sh
+++ llvm/build-llvm.sh
@@ -3,9 +3,9 @@
 cd $XTERN_ROOT/llvm
 rm -rf llvm-* install
 wget http://llvm.org/releases/2.7/llvm-2.7.tgz
-wget http://llvm.org/releases/2.7/llvm-gcc-4.2-2.7.source.tgz
+wget http://llvm.org/releases/2.7/llvm-gcc4.2-2.7-x86_64-linux.tar.bz2
 tar zxvf llvm-2.7.tgz
-tar zxvf llvm-gcc-4.2-2.7.source.tgz
+tar xJvf llvm-gcc4.2-2.7-x86_64-linux.tar.bz2
 
 if [ -z $1 ]; then
   ./scripts/mk-llvm
