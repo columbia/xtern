494d86d6fc1d95cb57fe39a36a9c29b1937e06f9
Heming Cui <heming@heming.(none)>
2013-01-17 23:29:16 -0500

    Nits to xtern scripts to support 32bits

--- llvm/build-llvm.sh
+++ llvm/build-llvm.sh
@@ -4,9 +4,16 @@ MACHINE=`uname -m`
 cd $XTERN_ROOT/llvm
 rm -rf llvm-* install
 wget http://llvm.org/releases/2.7/llvm-2.7.tgz
-wget http://llvm.org/releases/2.7/llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
+
+if [ $MACHINE = "x86_64" ]; then
+  wget http://llvm.org/releases/2.7/llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
+  tar xjvf llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
+else
+  wget http://llvm.org/releases/2.7/llvm-gcc4.2-2.7-$MACHINE-linux.tgz
+  tar xzvf llvm-gcc4.2-2.7-$MACHINE-linux.tgz
+fi
+
 tar zxvf llvm-2.7.tgz
-tar xjvf llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
 
 if [ -z $1 ]; then
   ./scripts/mk-llvm
