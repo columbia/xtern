0b58564b411a582c5cb3c1acde20e0f313a11d67
Yi-Hong Lin <yl2790@columbia.edu>
2013-02-08 18:06:52 -0500

    add script for extracting results

---
+++ eval/extract.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -d $XTERN_ROOT ]; then
+    echo "XTERN_ROOT is not defined"
+    exit 1
+fi
+
+if [ "$1" == "" ]; then
+    logs=$XTERN_ROOT/eval/current
+else
+    logs=$1
+fi
+
+if [ ! -d $logs ]; then
+    echo "$logs is not a folder!"
+    exit 1
+fi
+cd $logs
+
+find . -name stats.txt -print | cut -d "/" -f 2 | sort 
+
+find . -name stats.txt -print | sort | xargs -I {} bash -c "awk 'BEGIN{line=0;}{line++;if (line == 4)xtern_avg = \$2;if (line == 5)xtern_sem = \$2;if (line == 7)nondet_avg = \$2;if (line==8){print nondet_avg, \$2, xtern_avg, xtern_sem;line = 0;}}' {}; "
+
