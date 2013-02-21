d863bd05a4c6828f5a00733124b39b129a1e9a2b
Yi-Hong Lin <yl2790@columbia.edu>
2013-02-21 03:28:33 -0500

    fix splash2x.fmm

--- eval/extract.sh
+++ eval/extract.sh
@@ -19,5 +19,24 @@ cd $logs
 
 find . -name stats.txt -print | sort | cut -d "/" -f 2 
 
-find . -name stats.txt -print | sort | xargs -I {} bash -c "awk 'BEGIN{line=0;}{line++;if (line == 4)xtern_avg = \$2;if (line == 5)xtern_sem = \$2;if (line == 7)nondet_avg = \$2;if (line==8){print nondet_avg, \$2, xtern_avg, xtern_sem;line = 0;}}' {}; "
+find . -name stats.txt -print | sort | xargs -I {} bash -c \
+"awk '
+BEGIN{line=0; dthreads=0; dmp_o=0; dmp_b=0; dmp_pb=0; dmp_hb=0;}
+{
+    if (\$1 == \"dthreads:\") dthreads = 1;
+    if (\$1 == \"dmp_o:\") dmp_o = 1;
+    if (\$1 == \"dmp_b:\") dmp_b = 1;
+    if (\$1 == \"dmp_pb:\") dmp_pb = 1;
+    if (\$1 == \"dmp_hb:\") dmp_hb = 1;
+    line++;
+    if (line == 4) xtern_avg = \$2;
+    if (line == 5) xtern_sem = \$2;
+    if (line == 7) nondet_avg = \$2;
+    if (line == 8) nodet_sem = \$2;
+    if (line == 8){print nondet_avg, nodet_sem, xtern_avg, xtern_sem;}
+}
+END {
+    print dthreads, dmp_o, dmp_b, dmp_pb, dmp_hb
+}
+' {}; "
 
