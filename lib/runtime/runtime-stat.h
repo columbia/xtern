047800e0a0c235527e56c1a15ac3491e45dea451
Heming Cui <heming@heming.(none)>
2013-03-09 00:38:38 -0500

    Added runtime-stat for micro-events.

---
+++ lib/runtime/runtime-stat.h
@@ -0,0 +1,37 @@
+#ifndef __TERN_COMMON_RUNTIME_STAT_H
+#define __TERN_COMMON_RUNTIME_STAT_H
+
+#include <stdio.h>
+#include <iostream>
+
+namespace tern {
+class RuntimeStat {
+public:
+  long nDetPthreadSyncOp;
+  long nInterProcSyncOp;
+  long nLineupSucc;
+  long nLineupTimeout;
+  long nNonDetRegions;
+  long nNonDetPthreadSync;
+  
+public:
+  RuntimeStat() {
+    nDetPthreadSyncOp = 0;
+    nInterProcSyncOp = 0;
+    nLineupSucc = 0;
+    nLineupTimeout = 0;
+    nNonDetRegions = 0;
+    nNonDetPthreadSync = 0;    
+  }
+  void print() {
+    std::cout << "\n\nRuntimeStat:\n"
+      << "nDetPthreadSyncOp\t" << "nInterProcSyncOp\t" << "nLineupSucc\t" << "nLineupTimeout\t" << "nNonDetRegions\t" << "nNonDetPthreadSync\t" << "\n"    
+      << nDetPthreadSyncOp << "\t" << nInterProcSyncOp << "\t" << nLineupSucc << "\t" << nLineupTimeout << "\t" << nNonDetRegions << "\t" << nNonDetPthreadSync
+      << "\n\n";
+  }
+
+};
+}
+
+#endif
+
