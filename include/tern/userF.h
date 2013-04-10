e0e4eb0cbbcfed9d20897c2617e1b9f099971c62
Heming Cui <heming@heming.(none)>
2013-04-10 17:13:06 -0400

    Refactor hints.

--- include/tern/userF.h
+++ include/tern/userF.h
@@ -11,20 +11,20 @@ c----------------------------------------------------------
               integer(kind=c_int), VALUE :: opaque_type
               integer(kind=c_int), VALUE :: count
               integer(kind=c_int), VALUE :: timeout_turns
-          end subroutine tern_lineup_init
+          end subroutine soba_init
           subroutine soba_wait( opaque_type) bind ( c )
               use iso_c_binding
               integer(kind=c_int), VALUE :: opaque_type
-          end subroutine tern_lineup
+          end subroutine soba_wait
           subroutine pcs_enter() bind ( c )
               use iso_c_binding
-          end subroutine tern_non_det_start
+          end subroutine pcs_enter
           subroutine pcs_exit() bind ( c )
               use iso_c_binding
-          end subroutine tern_non_det_end
+          end subroutine pcs_exit
 	  subroutine pcs_barrier_exit(bar_id, cnt) bind ( c )
               use iso_c_binding
               integer(kind=c_int), VALUE :: bar_id
               integer(kind=c_int), VALUE :: cnt
-          end subroutine tern_non_det_barrier_end
+          end subroutine pcs_barrier_exit
       end interface
