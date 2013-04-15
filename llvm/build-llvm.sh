82b6fe7d31ee8a76433d0ecf29096beb8ebdbd3a
Yi-Hong Lin <yl2790@columbia.edu>
2013-04-15 12:26:40 -0400

    refine some mk script

--- llvm/build-llvm.sh
+++ llvm/build-llvm.sh
@@ -7,16 +7,16 @@ wget http://llvm.org/releases/2.7/llvm-2.7.tgz
 
 if [ $MACHINE == "x86_64" ]; then
   wget http://llvm.org/releases/2.7/llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
-  tar xjvf llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
+  tar jxf llvm-gcc4.2-2.7-$MACHINE-linux.tar.bz2
 else
   wget http://llvm.org/releases/2.7/llvm-gcc-4.2-2.7-$MACHINE-linux.tgz
-  tar xzvf llvm-gcc-4.2-2.7-$MACHINE-linux.tgz
+  tar zxf llvm-gcc-4.2-2.7-$MACHINE-linux.tgz
   mv llvm-gcc-4.2-2.7-$MACHINE-linux llvm-gcc4.2-2.7-$MACHINE-linux
 fi
 
 #exit;
 
-tar zxvf llvm-2.7.tgz
+tar zxf llvm-2.7.tgz
 
 if [ -z $1 ]; then
   ./scripts/mk-llvm
