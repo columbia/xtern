b63e39778ec3ff2e000b148b3ad982d6d15bbfcd
Yi-Hong Lin <yl2790@columbia.edu>
2013-04-16 22:09:46 -0400

    nits to template.cpp

--- lib/parrot/func_template.cpp
+++ lib/parrot/func_template.cpp
@@ -1,63 +1,60 @@
 #ifndef __SPEC_HOOK_FUNC_NAME
-extern "C" FUNC_RET_TYPE FUNC_NAME(ARGS_WITH_NAME){
-  typedef FUNC_RET_TYPE (*orig_func_type)(ARGS_WITHOUT_NAME);
+extern "C" FUNC_RET_TYPE FUNC_NAME(ARGS_WITH_NAME) {
+  typedef FUNC_RET_TYPE(*orig_func_type)(ARGS_WITHOUT_NAME);
 
   static orig_func_type orig_func;
-
-  void * handle;
-  FUNC_RET_TYPE ret;
-
-  if (!orig_func) {
-    if(!(handle=dlopen("LIB_PATH", RTLD_LAZY))) {
-      perror("dlopen");
-      abort();
-    }
-
-    orig_func = (orig_func_type) dlsym(handle, "FUNC_NAME");
-
-    if(dlerror()) {
-      perror("dlsym");
-      abort();
-    }
-
-    dlclose(handle);
-  }
 
 #ifdef __USE_TERN_RUNTIME
   if (Space::isApp() && options::DMT) {
 #ifdef PRINT_DEBUG
-    fprintf(stdout, "%04d: FUNC_NAME is hooked.\n", (int) pthread_self());
+    fprintf(stdout, "%04d: FUNC_NAME is hooked.\n", (int)pthread_self());
     print_stack();
     fflush(stdout);
 #endif
 
 #ifdef __NEED_INPUT_INSID
     void *eip = 0;
+    FUNC_RET_TYPE ret;
     if (options::dync_geteip) {
       Space::enterSys();
       eip = get_eip();
       Space::exitSys();
     }
-    ret = tern_FUNC_NAME((unsigned)(uint64_t) eip, ARGS_ONLY_NAME);
+    ret = tern_FUNC_NAME((unsigned)(uint64_t)eip, ARGS_ONLY_NAME);
 #else
     ret = tern_FUNC_NAME(ARGS_ONLY_NAME);
 #endif
 
 #ifdef PRINT_DEBUG
-    fprintf(stdout, "%04d: FUNC_NAME returned.\n", (int) pthread_self());
+    fprintf(stdout, "%04d: FUNC_NAME returned.\n", (int)pthread_self());
     fflush(stdout);
 #endif
     return ret;
-  } 
+  }
 #endif
+
+  if (!orig_func) {
+    void * handle;
+    if (!(handle = dlopen("LIB_PATH", RTLD_LAZY))) {
+      perror("dlopen");
+      abort();
+    }
+
+    orig_func = (orig_func_type)dlsym(handle, "FUNC_NAME");
+
+    if (dlerror()) {
+      perror("dlsym");
+      abort();
+    }
+    dlclose(handle);
+  }
 
 #ifdef PRINT_DEBUG
-  fprintf(stdout, "%04d: FUNC_NAME is called.\n", (int) pthread_self());
+  fprintf(stdout, "%04d: FUNC_NAME is called.\n", (int)pthread_self());
   fflush(stdout);
 #endif
-  ret = orig_func(ARGS_ONLY_NAME);
 
-  return ret;
+  return orig_func(ARGS_ONLY_NAME);
 }
 #endif
 
--- lib/parrot/void_func_template.cpp
+++ lib/parrot/void_func_template.cpp
@@ -1,26 +1,8 @@
 #ifndef __SPEC_HOOK_FUNC_NAME
-extern "C" void FUNC_NAME(ARGS_WITH_NAME){
+extern "C" void FUNC_NAME(ARGS_WITH_NAME) {
   typedef int (*orig_func_type)(ARGS_WITHOUT_NAME);
 
   static orig_func_type orig_func;
-
-  void * handle;
-
-  if (!orig_func) {
-    if(!(handle=dlopen("LIB_PATH", RTLD_LAZY))) {
-      perror("dlopen");
-      abort();
-    }
-
-    orig_func = (orig_func_type) dlsym(handle, "FUNC_NAME");
-
-    if(dlerror()) {
-      perror("dlsym");
-      abort();
-    }
-
-    dlclose(handle);
-  }
 
 #ifdef __USE_TERN_RUNTIME
   if (Space::isApp() && options::DMT) {
@@ -32,21 +14,38 @@ extern "C" void FUNC_NAME(ARGS_WITH_NAME){
       eip = get_eip();
       Space::exitSys();
     }
-    tern_FUNC_NAME((unsigned)(uint64_t) eip, ARGS_ONLY_NAME);
+    tern_FUNC_NAME((unsigned)(uint64_t)eip, ARGS_ONLY_NAME);
 #else
     tern_FUNC_NAME(ARGS_ONLY_NAME);
 #endif
 
 #ifdef PRINT_DEBUG
-    fprintf(stdout, "%04d: FUNC_NAME is hooked.\n", (int) pthread_self());
+    fprintf(stdout, "%04d: FUNC_NAME is hooked.\n", (int)pthread_self());
 #endif
   }
 #else
 #endif
 
+  if (!orig_func) {
+    void * handle;
+    if (!(handle = dlopen("LIB_PATH", RTLD_LAZY))) {
+      perror("dlopen");
+      abort();
+    }
+
+    orig_func = (orig_func_type)dlsym(handle, "FUNC_NAME");
+
+    if (dlerror()) {
+      perror("dlsym");
+      abort();
+    }
+    dlclose(handle);
+  }
+
 #ifdef PRINT_DEBUG
-  fprintf(stdout, "%04d: FUNC_NAME is hooked.\n", (int) pthread_self());
-#endif
+  fprintf(stdout, "%04d: FUNC_NAME is called.\n", (int)pthread_self());
+  fflush(stdout);
+#endif  
 
   orig_func(ARGS_ONLY_NAME);
 }
