49f7aefc791732af9a03e2ef0a99357340a326d4
Yi-Hong Lin <yl2790@columbia.edu>
2013-05-10 00:14:43 -0400

    minor fix for imagick perf

--- guess/gen.py
+++ guess/gen.py
@@ -44,8 +44,8 @@ def extract(dir):
     eval_id = dir.split('_')[0]
     name = dir.split('_')[2]
     # handle special names
-    if name in ['water', 'lu', 'ocean', 'matrix', 'word', 'string'] :
-        name = "_".join([name, dir.split('_')[3]])
+    if name in ['water', 'lu', 'ocean', 'matrix', 'word', 'string', 'convert', 'compare', 'mogrify'] :
+        name = "_".join([name]+dir.split('_')[3:])
     return eval_id, '{:<25}'.format(name)
 
 if __name__ == "__main__":
