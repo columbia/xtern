66da9fb27bed1873fd504eac7007e8e83b573e05
heming <heming@heming.(none)>
2013-08-14 03:40:11 -0400

    Nits, update submodule.

--- dync_hook/template.h
+++ dync_hook/template.h
@@ -1,4 +1,4 @@
-#include <list>
+#include <vector>
 
 // Only support i386 and x86_64.
 #if defined(__i386__)
@@ -33,7 +33,9 @@ struct sync_op_entry {
 };
 
 pthread_spinlock_t rdtsc_lock;
-std::list<sync_op_entry *> rdtsc_log;
+const size_t max_v_size = 100000000;
+std::vector<sync_op_entry *> rdtsc_log;
+size_t rdtsc_index = 0;
 
 void process_rdtsc_log(void) {
   printf("Storing rdtsc log...\n");
@@ -44,13 +46,14 @@ void process_rdtsc_log(void) {
   FILE *f = fopen(log_path, "w");
   assert(f);
 
-  std::list<sync_op_entry *>::iterator itr = rdtsc_log.begin();
-  for (; itr != rdtsc_log.end(); itr++) {
-    struct sync_op_entry *entry = *itr;
+  for (size_t i = 0; i < rdtsc_index; i++) {
+    struct sync_op_entry *entry = rdtsc_log[i];
     fprintf(f, "%u %s %s %llu\n", entry->tid, entry->op.c_str(), entry->op_suffix.c_str(), entry->clock);
     delete entry;
   }
   rdtsc_log.clear();
+  fflush(f);
+  fclose(f);
 }
 
 void record_rdtsc_op(const char *op_name, const char *op_suffix, int print_depth) {
@@ -58,18 +61,23 @@ void record_rdtsc_op(const char *op_name, const char *op_suffix, int print_depth
     if (rdtsc_log.size() == 0) {
       pthread_spin_init(&rdtsc_lock, 0);
       atexit(process_rdtsc_log);
+      rdtsc_log.reserve (max_v_size);
+      rdtsc_index = 0;
     }
 
-    pthread_spin_lock(&rdtsc_lock);
     struct sync_op_entry *entry = new struct sync_op_entry;    
     entry->tid = (unsigned)pthread_self();
-    for (int i = 0; i < print_depth; i++)
-      entry->op += "	";
+    //for (int i = 0; i < print_depth; i++)
+    //  entry.op += "	";
     entry->op += op_name;
     entry->op_suffix = op_suffix;
     entry->clock = rdtsc();
+
+    pthread_spin_lock(&rdtsc_lock);
+    assert(rdtsc_index < max_v_size && "Please pre-allocated a bigger vector size for rdtsc log.");
     //fprintf(rdtsc_log, "%u %s %s %llu\n", (unsigned)pthread_self(), op_name, op_suffix, rdtsc());
-    rdtsc_log.push_back(entry);
+    rdtsc_log[rdtsc_index] = entry;
+    rdtsc_index++;
     pthread_spin_unlock(&rdtsc_lock);
   }
   //fprintf(stderr, "%u : %s %s %llu\n", (unsigned)pthread_self(), op_name, op_suffix, rdtsc());
