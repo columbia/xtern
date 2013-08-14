5f7cb007b924a2508051cfda0431ea151a8152b2
heming <heming@heming.(none)>
2013-08-14 02:54:18 -0400

    Nits, update submodule.

--- dync_hook/template.h
+++ dync_hook/template.h
@@ -1,3 +1,5 @@
+#include <list>
+
 // Only support i386 and x86_64.
 #if defined(__i386__)
 static __inline__ unsigned long long rdtsc(void)
@@ -17,25 +19,57 @@ static __inline__ unsigned long long rdtsc(void)
 
 //__thread
 // per process.
-FILE *rdtsc_log = NULL;
+struct sync_op_entry {
+  unsigned tid;
+  std::string op;
+  std::string op_suffix;
+  unsigned long long clock;
+  sync_op_entry() {
+    tid = 0;
+    op = "";
+    op_suffix = "";
+    clock = 0;
+  }
+};
+
 pthread_spinlock_t rdtsc_lock;
+std::list<sync_op_entry *> rdtsc_log;
+
+void process_rdtsc_log(void) {
+  printf("Storing rdtsc log...\n");
+  char log_path[1024];
+  memset(log_path, 0, 1024);
+  snprintf(log_path, 1024, "%s/pself-pid-%u.txt", options::rdtsc_output_dir.c_str(), (unsigned)getpid());
+  mkdir(options::rdtsc_output_dir.c_str(), 0777);
+  FILE *f = fopen(log_path, "w");
+  assert(f);
+
+  std::list<sync_op_entry *>::iterator itr = rdtsc_log.begin();
+  for (; itr != rdtsc_log.end(); itr++) {
+    struct sync_op_entry *entry = *itr;
+    fprintf(f, "%u %s %s %llu\n", entry->tid, entry->op.c_str(), entry->op_suffix.c_str(), entry->clock);
+    delete entry;
+  }
+  rdtsc_log.clear();
+}
 
 void record_rdtsc_op(const char *op_name, const char *op_suffix, int print_depth) {
   if (options::record_rdtsc) {
-    if (rdtsc_log == NULL) {
-      char log_path[1024];
-      memset(log_path, 0, 1024);
-      snprintf(log_path, 1024, "%s/pself-pid-%u.txt", options::rdtsc_output_dir.c_str(), (unsigned)getpid());
-      mkdir(options::rdtsc_output_dir.c_str(), 0777);
-      rdtsc_log = fopen(log_path, "w");
-      assert(rdtsc_log);
+    if (rdtsc_log.size() == 0) {
       pthread_spin_init(&rdtsc_lock, 0);
+      atexit(process_rdtsc_log);
     }
-    // FIXME: should add a spin lock to protect here, maybe.
+
     pthread_spin_lock(&rdtsc_lock);
+    struct sync_op_entry *entry = new struct sync_op_entry;    
+    entry->tid = (unsigned)pthread_self();
     for (int i = 0; i < print_depth; i++)
-      fprintf(rdtsc_log, "	");
-    fprintf(rdtsc_log, "%u %s %s %llu\n", (unsigned)pthread_self(), op_name, op_suffix, rdtsc());
+      entry->op += "	";
+    entry->op += op_name;
+    entry->op_suffix = op_suffix;
+    entry->clock = rdtsc();
+    //fprintf(rdtsc_log, "%u %s %s %llu\n", (unsigned)pthread_self(), op_name, op_suffix, rdtsc());
+    rdtsc_log.push_back(entry);
     pthread_spin_unlock(&rdtsc_lock);
   }
   //fprintf(stderr, "%u : %s %s %llu\n", (unsigned)pthread_self(), op_name, op_suffix, rdtsc());
