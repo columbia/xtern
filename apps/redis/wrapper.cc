3fdfbcbe1daaebdb7217485851692e5a32355c83
heming <heming@heming.(none)>
2013-09-03 21:12:40 -0400

    Add wrappers for redis.

---
+++ apps/redis/wrapper.cc
@@ -0,0 +1,24 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int client_pid = fork();
+  if (client_pid == 0) {
+    //char *argv[] = { "/home/heming/rcs/smt+mc/xtern/apps/redis/redis-benchmark", "-n", "2", "-c", "2", "-t", "get" , NULL };
+    char *argv[] = { "/home/jsimsa/smt+mc/xtern/apps/redis/redis-benchmark", "-n", "2", "-t", "get" , NULL };
+    execv("/home/heming/rcs/smt+mc/xtern/apps/redis/redis-benchmark", argv);
+  }
+  sleep(0);
+  int server_pid = fork();
+  if (server_pid == 0) {
+    char *argv[] = { "/home/heming/rcs/smt+mc/xtern/apps/redis/redis-server", NULL };
+    execv("/home/heming/rcs/smt+mc/xtern/apps/redis/redis-server", argv);
+  }
+  waitpid(client_pid, NULL, 0);
+  kill(server_pid, SIGTERM);
+  waitpid(server_pid, NULL, 0);
+  return 0;
+}
+
