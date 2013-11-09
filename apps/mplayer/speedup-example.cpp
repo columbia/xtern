4040ad4e3549dfef27dd4673185d0dc1aae0b372
heming <heming@heming.(none)>
2013-11-09 02:10:01 -0500

    revert3.
    Revert "Added a mencoder-like speedup toy program."
    
    This reverts commit 8bc18a0f88539a68c3ddf26df8f0ecdf23c78a1f.

--- apps/mplayer/speedup-example.cpp
+++ apps/mplayer/speedup-example.cpp
@@ -32,7 +32,6 @@ volatile long long sum;
 long loops = 6e3;
 pthread_mutex_t mutex;
 pthread_cond_t cond;
-pthread_barrier_t bar;
 
 void set_affinity(int core_id) {
   cpu_set_t cpuset;
@@ -51,7 +50,6 @@ void* thread_func(void *arg) {
     pthread_cond_wait(&cond, &mutex);
     pthread_mutex_unlock(&mutex);
     soba_wait(0);
-    pthread_barrier_wait(&bar);
     for (long i = 0; i < loops; i++)
       sum += i*i*i*i*i*i;
     //fprintf(stderr, "compute thread %u %d\n", (unsigned)thread, sched_getcpu());
@@ -64,7 +62,6 @@ int main(int argc, char *argv[]) {
   pthread_t th[N];
   int ret;
   pthread_cond_init(&cond, NULL);
-  pthread_barrier_init(&bar, NULL, N);
 
   for(unsigned i=0; i<N; ++i) {
     ret  = pthread_create(&th[i], NULL, thread_func, (void*)i);
